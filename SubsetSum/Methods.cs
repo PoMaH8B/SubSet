065dc6f334deef7d83a3694a570d84ab87106f24
PoMaH8B <37907264+PoMaH8B@users.noreply.github.com>
2018-04-30 13:00:21 +0300

    Add files via upload

---
+++ SubsetSum/Methods.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubsetSum
+{
+    static class Methods
+    {
+        public static List<int> GenerateRandomList(int n, int min, int max)
+        {
+            Random rd = new Random();
+            List<int> res = new List<int>();
+            for(int i = 0; i < n; ++i)
+            {
+                res.Add(rd.Next(min, max + 1));
+            }
+            return res;
+        }
+
+        public static List<int> GreedySubsetSum(List<int> list, int sum, out int currSum)
+        {
+            var result = new List<int>();
+            currSum = 0;
+            foreach (int i in list.OrderByDescending(k => k).SkipWhile(k => k > sum))
+            {
+                currSum += i;
+                if (currSum > sum)
+                    return result;
+                else
+                    result.Add(i);
+            }
+            return result;
+        }
+
+        public static List<int> BrutSubsetSum(List<int> list, int sum)
+        {
+            try
+            {
+                return FindAllSubsets(list).First(set => set.Sum() == sum);
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+        }
+
+        private static IEnumerable<List<int>> FindAllSubsets(List<int> itemset)
+        {
+            long subsetCount = 1L << itemset.Count();
+            if(subsetCount == 1)
+            {
+                throw new ArgumentException("Invalid itemset count");
+            }
+            for (int i = 0; i < subsetCount; i++)
+            {
+                List<int> subset = new List<int>();
+                for (int bitIndex = 0; bitIndex < itemset.Count(); bitIndex++)
+                {
+                    if ((i & (1 << (itemset.Count - bitIndex - 1))) != 0)
+                    {
+                        subset.Add(itemset[bitIndex]);
+                    }
+                }
+                yield return subset;
+            }
+        }
+    }
+}
---
+++ SubsetSum/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubsetSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов" + Environment.NewLine + "> ");
+            int amount;
+            while (!Int32.TryParse(Console.ReadLine(), out amount))
+            {
+                Console.Write("Повторите ввод" + Environment.NewLine + "> ");
+            }
+
+            List<int> list = Methods.GenerateRandomList(amount, 10, 99);
+            Console.WriteLine(String.Join(", ", list));
+
+            Console.Write("Введите искомую сумму" + Environment.NewLine + "> ");
+            int sum;
+            while (!Int32.TryParse(Console.ReadLine(), out sum))
+            {
+                Console.Write("Повторите ввод" + Environment.NewLine + "> ");
+            }
+
+            DateTime start = DateTime.Now;
+            int greedySum;
+            Console.WriteLine("Жадный алгоритм запущен... ");
+            var res = Methods.GreedySubsetSum(list, sum, out greedySum);
+            Console.WriteLine("Жадный алгоритм: ");
+            Console.WriteLine(res == null ? "Не найдено" : String.Join(", ", res));
+            Console.WriteLine("Сумма: " + greedySum);
+            Console.Write("Затрачено времени: ");
+            Console.WriteLine(DateTime.Now - start);
+
+            start = DateTime.Now;
+            Console.WriteLine("Полный перебор запущен... ");
+            try
+            {
+                res = Methods.BrutSubsetSum(list, sum);
+                Console.WriteLine("Полный перебор: ");
+                Console.WriteLine(res == null ? "Не найдено" : String.Join(", ", res));
+                Console.Write("Затрачено времени: ");
+                Console.WriteLine(DateTime.Now - start);
+            }
+            catch
+            {
+                Console.WriteLine("Размер множества слишком большой для полного перебора!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
